589e4aa7fe8fe53ae86e319dd3e3026512b9c6aa
ArthurPV <arthurpontiervalois@gmail.com>
2026-01-10 10:46:55 -0500

    Try

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,34 +7,35 @@ set -o pipefail
 
 DOCKER_CI_DIR=$1
 
-sudo apt update
-sudo apt install -y  \
-	gcc-10 g++-10 \
-	ros-humble-image-transport \
-	ros-humble-builtin-interfaces \
-	ros-humble-std-msgs \
-	ros-humble-tf2 \
-	ros-humble-tf2-ros \
-	ros-humble-tf2-geometry-msgs \
-	ros-humble-geometry-msgs \
-	ros-nav-msgs \
-	ros-humble-nmea-msgs \
-	ros-humble-geographic-msgs \
-	ros-humble-sensor-msgs \
-	ros-humble-stereo-msgs \
-	ros-humble-zed-msgs \
-	ros-humble-std-srvs \
-	ros-humble-cob-srvs \
-	ros-humble-diagnostic-msgs \
-	ros-humble-diagnostic-updater \
-	ros-humble-visualization-msgs \
-	ros-humble-shape-msgs \
-	ros-humble-robot-localization
-
-$DOCKER_CI_DIR/scripts/install_cuda.sh
+# sudo apt update
+# sudo apt install -y  \
+# 	gcc-10 g++-10 \
+# 	ros-humble-image-transport \
+# 	ros-humble-builtin-interfaces \
+# 	ros-humble-std-msgs \
+# 	ros-humble-tf2 \
+# 	ros-humble-tf2-ros \
+# 	ros-humble-tf2-geometry-msgs \
+# 	ros-humble-geometry-msgs \
+# 	ros-nav-msgs \
+# 	ros-humble-nmea-msgs \
+# 	ros-humble-geographic-msgs \
+# 	ros-humble-sensor-msgs \
+# 	ros-humble-stereo-msgs \
+# 	ros-humble-zed-msgs \
+# 	ros-humble-std-srvs \
+# 	ros-humble-cob-srvs \
+# 	ros-humble-diagnostic-msgs \
+# 	ros-humble-diagnostic-updater \
+# 	ros-humble-visualization-msgs \
+# 	ros-humble-shape-msgs \
+# 	ros-humble-robot-localization
+# 
+# $DOCKER_CI_DIR/scripts/install_cuda.sh
 
 source /opt/ros/humble/setup.bash
 
+# TODO: Maybe remove that
 git clone https://github.com/stereolabs/zed-ros2-wrapper.git
 
 cd zed-ros2-wrapper
